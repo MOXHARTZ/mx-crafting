4bc71af0a74ff5c1ce48e895c0748c03766017aa
MOXHAFOREVA <70913098+MOXHAFOREVA@users.noreply.github.com>
2021-04-12 18:41:40 +0300

    Update server.lua

--- mx-crafting/server.lua
+++ mx-crafting/server.lua
@@ -9,18 +9,18 @@ AddEventHandler('mx-crafting:craft', function(z, a, craftedItem, craftedItemcoun
         if xPlayer.getInventoryItem(a[1].reqitem).count >= a[1].count then
             xPlayer.removeInventoryItem(a[1].reqitem, a[1].count)
             xPlayer.addInventoryItem(craftedItem, craftedItemcount)
-            xPlayer.showNotification("Eşyayı başarıyla craftladın")
+            xPlayer.showNotification("Crafted Item !")
         else
-            xPlayer.showNotification("Yeterli malzeme yok")
+            xPlayer.showNotification("There is not enough material.")
         end
     elseif z == 2 then
         if xPlayer.getInventoryItem(a[1].reqitem).count >= a[1].count and xPlayer.getInventoryItem(a[2].reqitem).count >= a[2].count then
             xPlayer.removeInventoryItem(a[1].reqitem, a[1].count)
             xPlayer.removeInventoryItem(a[2].reqitem, a[2].count)
             xPlayer.addInventoryItem(craftedItem, craftedItemcount)
-            xPlayer.showNotification("Eşyayı başarıyla craftladın")
+            xPlayer.showNotification("Crafted Item !")
         else
-            xPlayer.showNotification("Yeterli malzeme yok")
+            xPlayer.showNotification("There is not enough material.")
         end
     elseif z == 3 then
         if xPlayer.getInventoryItem(a[1].reqitem).count >= a[1].count and xPlayer.getInventoryItem(a[2].reqitem).count >= a[2].count and xPlayer.getInventoryItem(a[3].reqitem).count >= a[3].count then
@@ -28,9 +28,9 @@ AddEventHandler('mx-crafting:craft', function(z, a, craftedItem, craftedItemcoun
             xPlayer.removeInventoryItem(a[2].reqitem, a[2].count)
             xPlayer.removeInventoryItem(a[3].reqitem, a[3].count)
             xPlayer.addInventoryItem(craftedItem, craftedItemcount)
-            xPlayer.showNotification("Eşyayı başarıyla craftladın")
+            xPlayer.showNotification("Crafted Item !")
         else
-            xPlayer.showNotification("Yeterli malzeme yok")
+            xPlayer.showNotification("There is not enough material.")
         end
     elseif z == 4 then
         if xPlayer.getInventoryItem(a[1].reqitem).count >= a[1].count and xPlayer.getInventoryItem(a[2].reqitem).count >= a[2].count and xPlayer.getInventoryItem(a[3].reqitem).count >= a[3].count and xPlayer.getInventoryItem(a[4].reqitem).count >= a[4].count then
@@ -39,9 +39,9 @@ AddEventHandler('mx-crafting:craft', function(z, a, craftedItem, craftedItemcoun
             xPlayer.removeInventoryItem(a[3].reqitem, a[3].count)
             xPlayer.removeInventoryItem(a[4].reqitem, a[4].count)
             xPlayer.addInventoryItem(craftedItem, craftedItemcount)
-            xPlayer.showNotification("Eşyayı başarıyla craftladın")
+            xPlayer.showNotification("Crafted Item !")
         else
-            xPlayer.showNotification("Yeterli malzeme yok")
+            xPlayer.showNotification("There is not enough material.")
         end
     end
 end)
