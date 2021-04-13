0f3d696ea8f9bb65da2269a1dd75b7e510f3ce76
MOXHAFOREVA <70913098+MOXHAFOREVA@users.noreply.github.com>
2021-04-13 14:28:51 +0300

    Update server.lua

--- mx-crafting/server.lua
+++ mx-crafting/server.lua
@@ -1,47 +1,29 @@
 ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-RegisterServerEvent('mx-crafting:craft')
-AddEventHandler('mx-crafting:craft', function(z, a, craftedItem, craftedItemcount)
-    local src = source
-    local xPlayer = ESX.GetPlayerFromId(src)
-    if z == 1 then
-        if xPlayer.getInventoryItem(a[1].reqitem).count >= a[1].count then
-            xPlayer.removeInventoryItem(a[1].reqitem, a[1].count)
-            xPlayer.addInventoryItem(craftedItem, craftedItemcount)
-            xPlayer.showNotification("Crafted Item !")
+ESX.RegisterServerCallback('mx-crafting:hasItem', function(source, cb, Craft)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local heart = 0
+    for i = 1, #Craft do
+        if xPlayer.getInventoryItem(Craft[i].reqitem) and xPlayer.getInventoryItem(Craft[i].reqitem).count >= Craft[i].count then
+            heart = heart + 1
         else
-            xPlayer.showNotification("There is not enough material.")
-        end
-    elseif z == 2 then
-        if xPlayer.getInventoryItem(a[1].reqitem).count >= a[1].count and xPlayer.getInventoryItem(a[2].reqitem).count >= a[2].count then
-            xPlayer.removeInventoryItem(a[1].reqitem, a[1].count)
-            xPlayer.removeInventoryItem(a[2].reqitem, a[2].count)
-            xPlayer.addInventoryItem(craftedItem, craftedItemcount)
-            xPlayer.showNotification("Crafted Item !")
-        else
-            xPlayer.showNotification("There is not enough material.")
-        end
-    elseif z == 3 then
-        if xPlayer.getInventoryItem(a[1].reqitem).count >= a[1].count and xPlayer.getInventoryItem(a[2].reqitem).count >= a[2].count and xPlayer.getInventoryItem(a[3].reqitem).count >= a[3].count then
-            xPlayer.removeInventoryItem(a[1].reqitem, a[1].count)
-            xPlayer.removeInventoryItem(a[2].reqitem, a[2].count)
-            xPlayer.removeInventoryItem(a[3].reqitem, a[3].count)
-            xPlayer.addInventoryItem(craftedItem, craftedItemcount)
-            xPlayer.showNotification("Crafted Item !")
-        else
-            xPlayer.showNotification("There is not enough material.")
-        end
-    elseif z == 4 then
-        if xPlayer.getInventoryItem(a[1].reqitem).count >= a[1].count and xPlayer.getInventoryItem(a[2].reqitem).count >= a[2].count and xPlayer.getInventoryItem(a[3].reqitem).count >= a[3].count and xPlayer.getInventoryItem(a[4].reqitem).count >= a[4].count then
-            xPlayer.removeInventoryItem(a[1].reqitem, a[1].count)
-            xPlayer.removeInventoryItem(a[2].reqitem, a[2].count)
-            xPlayer.removeInventoryItem(a[3].reqitem, a[3].count)
-            xPlayer.removeInventoryItem(a[4].reqitem, a[4].count)
-            xPlayer.addInventoryItem(craftedItem, craftedItemcount)
-            xPlayer.showNotification("Crafted Item !")
-        else
-            xPlayer.showNotification("There is not enough material.")
+            xPlayer.showNotification("There is not enough material. Material : "..Craft[i].reqitem..' Material amount : '..Craft[i].count)
+            cb(false)
+            break
         end
     end
+    if heart >= #Craft then
+        cb(true)
+    end
+end)
+
+RegisterServerEvent('mx-crafting:craft')
+AddEventHandler('mx-crafting:craft', function(Craft, craftedItem, craftedItemcount)
+    local src = source
+    local xPlayer = ESX.GetPlayerFromId(src)
+    for n = 1, #Craft do
+        xPlayer.removeInventoryItem(Craft[n].reqitem, Craft[n].count)
+    end
+    xPlayer.addInventoryItem(craftedItem, craftedItemcount)
 end)
